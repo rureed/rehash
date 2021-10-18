c3bcffd7a7527a330ba0b415ac83cb4d03b1ae24
rureed <rureed@gmail.com>
2021-10-18 13:58:49 -0400

    using button

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("click1").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("click1").innerHTML = "Well Done!!";
+  console.log("it worked");
+}
+
+// function buttonFunction() {
+//     document.getElementById("click1").innerHTML = "Well done!!";
+//     console.log("Shazam!")
+
+// }
